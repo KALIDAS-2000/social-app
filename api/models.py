ea7d14407390c996a13245cd713fd58ae5d4e4cb
kalidas <kalidasmanu1121@gmail.com>
2022-08-29 14:39:54 +0530

    create app

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+
+class Posts(models.Model):
+    title=models.CharField(max_length=120)
+    description=models.CharField(max_length=120)
+    image=models.ImageField(upload_to="postimages",null=True)
+    user=models.CharField(max_length=120)
+    date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
